e25776848a9d9518304201843593efef89da2e9c
RufengSama <535348743@qq.com>
2023-07-11 10:11:26 +0800

    2023-07-11 updateTest

--- acmer/src/test/java/com/buct/acmer/service/StudentServiceTestCase.java
+++ acmer/src/test/java/com/buct/acmer/service/StudentServiceTestCase.java
@@ -1,6 +1,8 @@
 package com.buct.acmer.service;
 
 import com.buct.acmer.entity.Student;
+import com.buct.acmer.service.impl.StudentServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,4 +11,9 @@ public class StudentServiceTestCase {
 
     @Autowired
     private IStudentService studentService;
+
+    @Test
+    void testGet(){
+        System.out.println(studentService.getById(2));
+    }
 }
